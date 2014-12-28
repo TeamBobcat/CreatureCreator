85271ab465ab687ca9c235883f7c293a182e7c9a
Tahmoor Hussain <itahmoor-27@hotmail.co.uk>
2014-12-28 15:39:21 +0000

    forgot to use GIT initially, now using. procedurally generated skeleton of different segments of body parts done

---
+++ CreatureCreator/Assets/Procedural/BodyPart.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyPart : MonoBehaviour
+{
+    
+   public void create_bone(Vector3 position, Mesh part, float x_size, float y_size, float z_size)
+    {
+       
+            #region Vertices_
+        Vector3 p0 = new Vector3(position.x + x_size, position.y, position.z);
+        Vector3 p1 = new Vector3(position.x, position.y, position.z);
+        Vector3 p2 = new Vector3(position.x, position.y, position.z + z_size);
+        Vector3 p3 = new Vector3(position.x + x_size, position.y, position.z + z_size);
+
+        Vector3 p4 = new Vector3(position.x + x_size, position.y + y_size, position.z);
+        Vector3 p5 = new Vector3(position.x, position.y + y_size, position.z);
+        Vector3 p6 = new Vector3(position.x, position.y + y_size, position.z + z_size);
+        Vector3 p7 = new Vector3(position.x + x_size, position.y + y_size, position.z + z_size);
+
+            Vector3[] vertices = new Vector3[]
+{
+	// Bottom
+	p0, p1, p2, p3,
+ 
+	// Left
+	p7, p4, p0, p3,
+ 
+	// Front
+	p4, p5, p1, p0,
+ 
+	// Back
+	p6, p7, p3, p2,
+ 
+	// Right
+	p5, p6, p2, p1,
+ 
+	// Top
+	p7, p6, p5, p4
+};
+            #endregion
+
+            #region Normales
+            Vector3 up = Vector3.up;
+            Vector3 down = Vector3.down;
+            Vector3 front = Vector3.forward;
+            Vector3 back = Vector3.back;
+            Vector3 left = Vector3.left;
+            Vector3 right = Vector3.right;
+
+            Vector3[] normales = new Vector3[]
+{
+	// Bottom
+	down, down, down, down,
+ 
+	// Left
+	left, left, left, left,
+ 
+	// Front
+	front, front, front, front,
+ 
+	// Back
+	back, back, back, back,
+ 
+	// Right
+	right, right, right, right,
+ 
+	// Top
+	up, up, up, up
+};
+            #endregion
+
+            #region UVs
+            Vector2 _00 = new Vector2(0f, 0f);
+            Vector2 _10 = new Vector2(1f, 0f);
+            Vector2 _01 = new Vector2(0f, 1f);
+            Vector2 _11 = new Vector2(1f, 1f);
+
+            Vector2[] uvs = new Vector2[]
+{
+	// Bottom
+	_11, _01, _00, _10,
+ 
+	// Left
+	_11, _01, _00, _10,
+ 
+	// Front
+	_11, _01, _00, _10,
+ 
+	// Back
+	_11, _01, _00, _10,
+ 
+	// Right
+	_11, _01, _00, _10,
+ 
+	// Top
+	_11, _01, _00, _10,
+};
+            #endregion
+
+            #region Triangles
+            int[] triangles = new int[]
+{
+	// Bottom
+	3, 1, 0,
+	3, 2, 1,			
+ 
+	// Left
+	3 + 4 * 1, 1 + 4 * 1, 0 + 4 * 1,
+	3 + 4 * 1, 2 + 4 * 1, 1 + 4 * 1,
+ 
+	// Front
+	3 + 4 * 2, 1 + 4 * 2, 0 + 4 * 2,
+	3 + 4 * 2, 2 + 4 * 2, 1 + 4 * 2,
+ 
+	// Back
+	3 + 4 * 3, 1 + 4 * 3, 0 + 4 * 3,
+	3 + 4 * 3, 2 + 4 * 3, 1 + 4 * 3,
+ 
+	// Right
+	3 + 4 * 4, 1 + 4 * 4, 0 + 4 * 4,
+	3 + 4 * 4, 2 + 4 * 4, 1 + 4 * 4,
+ 
+	// Top
+	3 + 4 * 5, 1 + 4 * 5, 0 + 4 * 5,
+	3 + 4 * 5, 2 + 4 * 5, 1 + 4 * 5,
+ 
+};
+            #endregion
+        
+ 
+            part.vertices = vertices;
+            part.normals = normales;
+            part.uv = uvs;
+            part.triangles = triangles;
+
+    }
+
+    public void create_thigh(Vector3 position, Mesh part, float x_size, float y_size, float z_size)
+    {
+       
+            #region Vertices_
+        Vector3 p0 = new Vector3(position.x + x_size - 0.1f, position.y, position.z + 0.1f);
+        Vector3 p1 = new Vector3(position.x + 0.1f, position.y, position.z + 0.1f);
+        Vector3 p2 = new Vector3(position.x + 0.1f, position.y, position.z + z_size - 0.1f);
+        Vector3 p3 = new Vector3(position.x + x_size - 0.1f, position.y, position.z + z_size - 0.1f);
+
+        Vector3 p4 = new Vector3(position.x + x_size, position.y + y_size, position.z);
+        Vector3 p5 = new Vector3(position.x, position.y + y_size, position.z);
+        Vector3 p6 = new Vector3(position.x, position.y + y_size, position.z + z_size);
+        Vector3 p7 = new Vector3(position.x + x_size, position.y + y_size, position.z + z_size);
+
+            Vector3[] vertices = new Vector3[]
+{
+	// Bottom
+	p0, p1, p2, p3,
+ 
+	// Left
+	p7, p4, p0, p3,
+ 
+	// Front
+	p4, p5, p1, p0,
+ 
+	// Back
+	p6, p7, p3, p2,
+ 
+	// Right
+	p5, p6, p2, p1,
+ 
+	// Top
+	p7, p6, p5, p4
+};
+            #endregion
+
+            #region Normales
+            Vector3 up = Vector3.up;
+            Vector3 down = Vector3.down;
+            Vector3 front = Vector3.forward;
+            Vector3 back = Vector3.back;
+            Vector3 left = Vector3.left;
+            Vector3 right = Vector3.right;
+
+            Vector3[] normales = new Vector3[]
+{
+	// Bottom
+	down, down, down, down,
+ 
+	// Left
+	left, left, left, left,
+ 
+	// Front
+	front, front, front, front,
+ 
+	// Back
+	back, back, back, back,
+ 
+	// Right
+	right, right, right, right,
+ 
+	// Top
+	up, up, up, up
+};
+            #endregion
+
+            #region UVs
+            Vector2 _00 = new Vector2(0f, 0f);
+            Vector2 _10 = new Vector2(1f, 0f);
+            Vector2 _01 = new Vector2(0f, 1f);
+            Vector2 _11 = new Vector2(1f, 1f);
+
+            Vector2[] uvs = new Vector2[]
+{
+	// Bottom
+	_11, _01, _00, _10,
+ 
+	// Left
+	_11, _01, _00, _10,
+ 
+	// Front
+	_11, _01, _00, _10,
+ 
+	// Back
+	_11, _01, _00, _10,
+ 
+	// Right
+	_11, _01, _00, _10,
+ 
+	// Top
+	_11, _01, _00, _10,
+};
+            #endregion
+
+            #region Triangles
+            int[] triangles = new int[]
+{
+	// Bottom
+	3, 1, 0,
+	3, 2, 1,			
+ 
+	// Left
+	3 + 4 * 1, 1 + 4 * 1, 0 + 4 * 1,
+	3 + 4 * 1, 2 + 4 * 1, 1 + 4 * 1,
+ 
+	// Front
+	3 + 4 * 2, 1 + 4 * 2, 0 + 4 * 2,
+	3 + 4 * 2, 2 + 4 * 2, 1 + 4 * 2,
+ 
+	// Back
+	3 + 4 * 3, 1 + 4 * 3, 0 + 4 * 3,
+	3 + 4 * 3, 2 + 4 * 3, 1 + 4 * 3,
+ 
+	// Right
+	3 + 4 * 4, 1 + 4 * 4, 0 + 4 * 4,
+	3 + 4 * 4, 2 + 4 * 4, 1 + 4 * 4,
+ 
+	// Top
+	3 + 4 * 5, 1 + 4 * 5, 0 + 4 * 5,
+	3 + 4 * 5, 2 + 4 * 5, 1 + 4 * 5,
+ 
+};
+            #endregion
+        
+ 
+            part.vertices = vertices;
+            part.normals = normales;
+            part.uv = uvs;
+            part.triangles = triangles;
+
+    }
+
+}
---
+++ CreatureCreator/Assets/Procedural/Creature.cs
@@ -0,0 +1,283 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : BodyPart
+{
+   
+    Vector3 position;
+    Rigidbody rigid;
+    BoxCollider collider;
+
+    Mesh body_part = new Mesh();
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject parent = GameObject.Find("Creature");
+        load_legs(parent, 2);
+        load_body(parent, 3);
+        load_arms(parent, 2);
+        load_neck(parent, 2);
+        load_tail(parent, 4);
+        //  load_wings();
+
+     //  rigid = parent.AddComponent<Rigidbody>();
+      // collider = parent.AddComponent<BoxCollider>();
+
+    }
+
+    void load_legs(GameObject obj, int number_of_segments)
+    {
+        position = new Vector3(0.1f, 0.0f, 0.1f);
+        GameObject legs = new GameObject("Legs");
+        legs.transform.parent = obj.transform;
+
+        GameObject section = gameObject;
+
+        int number_of_legs = 2;
+
+        for (int i = 0; i < number_of_legs; i++)
+        {
+            position.y = 0.0f;
+
+            if (i == 0)
+            {
+                section = new GameObject("Left Leg");
+                section.transform.parent = legs.transform;
+            }
+
+            if (i == 1)
+            {
+                section = new GameObject("Right Leg");
+                section.transform.parent = legs.transform;
+            }
+
+            for (int j = 0; j < number_of_segments; j++)
+            {
+
+                GameObject segment = new GameObject("Segment: " + (j + 1));
+                segment.transform.parent = section.transform;
+                create_mesh(segment);
+                      
+                if (j == 0)
+                {
+                    create_bone(position, body_part, 0.3f, 1.0f, 0.3f);
+                }
+
+                if (j == 1)
+                {
+                    create_thigh(position, body_part, 0.5f, 1.0f, 0.5f);
+                }
+
+                body_part.RecalculateBounds();
+                body_part.Optimize();
+
+                position.y = body_part.bounds.max.y;
+
+            }   
+            position.x += 0.5f;
+        }
+
+        GameObject shin = GameObject.Find("Left Leg/Segment: 1");
+        GameObject left_thigh = GameObject.Find("Left Leg/Segment: 2");
+      
+       // shin.transform.position = left_thigh.renderer.bounds.max;
+    }
+
+    void load_body(GameObject obj, int number_of_segments)
+    {
+        position.x = 0.1f; position.z = 0.1f;
+        GameObject body = new GameObject("Body");
+        body.transform.parent = obj.transform;
+
+        GameObject section = gameObject;
+        int number_of_sections = 3; //upper, mid, lower torso
+
+        for (int i = 0; i < number_of_sections; i++)
+        {
+
+            if (i == 0)
+            {
+                section = new GameObject("Lower Torso");
+                section.transform.parent = body.transform;
+            }
+
+            if (i == 1)
+            {
+                section = new GameObject("Mid Torso");
+                section.transform.parent = body.transform;
+            }
+
+            if (i == 2)
+            {
+                section = new GameObject("Upper Torso");
+                section.transform.parent = body.transform;
+            }
+
+            for (int j = 0; j < number_of_segments; j++)
+            {
+                GameObject segment = new GameObject("Segment: " + (j + 1));
+                segment.transform.parent = section.transform;
+                create_mesh(segment);
+                create_bone(position, body_part, 0.5f, 1.0f, 0.5f);
+
+                body_part.RecalculateBounds();
+                body_part.Optimize();
+
+                if (j == 0) { 
+           
+                    position.x += 0.5f;
+                
+                }
+                else if (j == 1)
+                {
+                    position.x -= 0.5f * 2;
+                }
+            }
+            position.x = 0.1f;
+            position.y = body_part.bounds.max.y;
+        }
+    }
+
+    void load_arms(GameObject obj, int number_of_segments)
+    {
+
+        GameObject right_upper_torso = GameObject.Find("Mid Torso/Segment: 3");
+        GameObject left_upper_torso = GameObject.Find("Mid Torso/Segment: 2");
+        position.y = right_upper_torso.renderer.bounds.max.y;
+        
+
+        GameObject arms = new GameObject("Arms");
+        arms.transform.parent = obj.transform;
+
+        GameObject section = gameObject;
+        int number_of_arms = 2; //upper and lower arm
+
+        for (int i = 0; i < number_of_arms; i++)
+        {
+
+            if (i == 0)
+            {
+                section = new GameObject("Right arm");
+                section.transform.parent = arms.transform;
+                position.x = right_upper_torso.renderer.bounds.min.x;
+
+            }
+
+            if (i == 1)
+            {
+                section = new GameObject("Left Arm");
+                section.transform.parent = arms.transform;
+                position.x = left_upper_torso.renderer.bounds.max.x;
+            }
+
+
+            for (int j = 0; j < number_of_segments; j++)
+            {
+                GameObject segment = new GameObject("Segment: " + (j + 1));
+                segment.transform.parent = section.transform;
+                if (i == 0)
+                {
+                    create_mesh(segment);
+                    create_bone(position, body_part, -1.0f, 0.2f, 0.2f);
+
+                    body_part.RecalculateBounds();
+                    body_part.Optimize();
+
+                    position.x = body_part.bounds.min.x;
+                }
+
+                if (i == 1)
+                {
+                    create_mesh(segment);
+                    create_bone(position, body_part, 1.0f, 0.2f, 0.2f);
+
+                    body_part.RecalculateBounds();
+                    body_part.Optimize();
+
+                    position.x = body_part.bounds.max.x;
+                }
+            }            
+
+        }
+    }
+
+    void load_neck(GameObject obj, int number_of_segments)
+    {
+        GameObject mid_upper_torso = GameObject.Find("Upper Torso/Segment: 1");
+        position.y = mid_upper_torso.renderer.bounds.max.y;
+        position.x = mid_upper_torso.renderer.bounds.min.x;
+
+        GameObject neck = new GameObject("Neck");
+        neck.transform.parent = obj.transform;
+
+            for (int j = 0; j < number_of_segments; j++)
+            {
+                GameObject segment = new GameObject("Segment: " + (j + 1));
+                segment.transform.parent = neck.transform;
+            
+                create_mesh(segment);
+                create_bone(position, body_part, 0.5f, 0.2f, 0.5f);
+
+                body_part.RecalculateBounds();
+                body_part.Optimize();
+
+                position.y = body_part.bounds.max.y;
+            }            
+    }
+
+    void load_tail(GameObject obj, int number_of_segments)
+    {
+        GameObject mid_lower_torso = GameObject.Find("Lower Torso/Segment: 1");
+        position.y = mid_lower_torso.renderer.bounds.min.y;
+        position.x = mid_lower_torso.renderer.bounds.min.x;
+        position.z = mid_lower_torso.renderer.bounds.min.z;
+
+        GameObject tail = new GameObject("Tail");
+        tail.transform.parent = obj.transform;
+
+        for (int j = 0; j < number_of_segments; j++)
+        {
+            GameObject segment = new GameObject("Segment: " + (j + 1));
+            segment.transform.parent = tail.transform;
+
+            create_mesh(segment);
+            create_bone(position, body_part, 0.1f, 0.1f, -1.0f);
+
+            body_part.RecalculateBounds();
+            body_part.Optimize();
+            position.z = body_part.bounds.min.z;
+        }   
+    }
+
+
+    void load_wings()
+    {
+
+
+
+    }
+
+    void create_mesh(GameObject obj)
+    {
+        MeshFilter filter = obj.AddComponent<MeshFilter>();
+        MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
+        renderer.material = new Material(Shader.Find("Diffuse"));
+        body_part = filter.mesh;
+        body_part.Clear();
+
+    }
+
+    void Update()
+    {
+        Vector3 jump = new Vector3 (0.0f, 100.0f, 0.0f);
+        if (Input.GetKeyDown("space"))
+        {
+            rigid.AddForce(jump);
+        }
+	}
+ }
+
+
